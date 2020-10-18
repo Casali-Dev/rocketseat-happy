f80a77596a3cadb1460cf1ff9656c35ad7113252
Casali-Code <guilhermecasali.dev@gmail.com>
2020-10-18 04:56:12 -0300

    Start of Day #3, improved front end and created sidebar component

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/map-marker.svg'
 
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 
 import '../styles/pages/orphanages-map.css';
+import { map } from 'leaflet';
+import mapIcon from '../utils/mapIcon';
+
 
 function OrphanagesMap() {
     return(
@@ -33,9 +36,21 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer url={`http://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+                
+                <Marker
+                    icon={mapIcon}
+                    position={[-30.0709206,-51.1673671]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={20} color="#FFF" />
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
